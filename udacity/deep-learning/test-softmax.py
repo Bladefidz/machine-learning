ae2ab614e1561dfcefdc42202e5d4b724e415400
bladefidz <gamerhacking@gmail.com>
2017-10-16 21:36:44 +0700

    Fix func name missmatch

--- udacity/deep-learning/test-softmax.py
+++ udacity/deep-learning/test-softmax.py
@@ -9,5 +9,5 @@ print(lc.softmax(scores))
 # Plot softmax curves
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
-plt.plot(x, softmax(scores).T, linewidth=2)
+plt.plot(x, lc.softmax(scores).T, linewidth=2)
 plt.show()

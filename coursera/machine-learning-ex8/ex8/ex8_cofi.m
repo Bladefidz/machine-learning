f64d65c769090518ad3b2a3e5bbd861e6c4e21e1
Bladefidz <gamerhacking@gmail.com>
2018-03-07 07:58:48 +0700

    Last coursera course!

--- coursera/machine-learning-ex8/ex8/ex8_cofi.m
+++ coursera/machine-learning-ex8/ex8/ex8_cofi.m
@@ -35,7 +35,9 @@ fprintf('Average rating for movie 1 (Toy Story): %f / 5\n\n', ...
         mean(Y(1, R(1, :))));
 
 %  We can "visualize" the ratings matrix by plotting it with imagesc
-imagesc(Y);
+%imagesc(Y);
+[m n] = size(Y);
+plot(1:n, 1:5);
 ylabel('Movies');
 xlabel('Users');
 
